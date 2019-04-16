2f25ce532595a0037321a88e10f13e4b7afec4ce
mikim190 <mykim.tran297@gmai.com>
2019-04-15 21:03:11 -0700

    Refactored dancer and blinky dancer. Working on CSS and adding our own style

--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,23 @@
+//Refactor Dancer Class, in pseudoclassical style
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps) {
 
-  var dancer = {};
-
-  // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
-
-  dancer.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
-
-  dancer.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
-
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
+var Dancer = function (top, left, timeBetweenSteps) {
+  this.$node = $('<span class="dancer"></span>');
+  this.setPosition(top, left);
+  this.step(timeBetweenSteps);
 };
+
+//Schedule next step for each dancer
+Dancer.prototype.step = function (time) {
+  setTimeout(this.step.bind(this), time);
+
+};
+//Set position for each dancer
+Dancer.prototype.setPosition = function (p1, p2) {
+  var styleSettings = {
+    top: p1,
+    left: p2
+  };
+  this.$node.css(styleSettings);
+};
+
